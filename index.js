813cc366b6d9731146487335016dd308134b2580
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2024-02-10 11:20:49 +0200

    feat: add basic routing

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/tasks', (req, res) => {
+  res.json([]);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
