bade291338560ca8826a4acb1ec7adce82ad08ee
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2024-02-10 11:55:03 +0200

    feat: add CORS middleware and 404 handler

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -9,6 +12,12 @@ app.get('/', (req, res) => {
 
 app.get('/tasks', (req, res) => {
   res.json([]);
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Not found',
+  });
 });
 
 app.listen(port, () => {
