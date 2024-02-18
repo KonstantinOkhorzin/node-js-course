f5429001ea49545949b6c4fc849f4b09da2afbab
Kostiantyn Okhorzin <konstantinokhorzin@gmail.com>
2024-02-18 07:47:44 +0200

    feat: add isValidId middleware

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -3,19 +3,25 @@ import express from 'express';
 import tasksController from '../../controllers/tasks.js';
 import { addSchema, updateSchema, updateCompletedSchema } from '../../schemas/tasks.js';
 import validateBody from '../../middlewares/validateBody.js';
+import isValidId from '../../middlewares/isValidId.js';
 
 const router = express.Router();
 
 router.get('/', tasksController.getAll);
 
-router.get('/:id', tasksController.getById);
+router.get('/:id', isValidId, tasksController.getById);
 
 router.post('/', validateBody(addSchema), tasksController.add);
 
-router.patch('/:id/completed', validateBody(updateCompletedSchema), tasksController.updateById);
+router.patch(
+  '/:id/completed',
+  isValidId,
+  validateBody(updateCompletedSchema),
+  tasksController.updateById
+);
 
-router.put('/:id', validateBody(updateSchema), tasksController.updateById);
+router.put('/:id', isValidId, validateBody(updateSchema), tasksController.updateById);
 
-router.delete('/:id', tasksController.deleteById);
+router.delete('/:id', isValidId, tasksController.deleteById);
 
 export default router;
